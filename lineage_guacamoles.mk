e8803f0d11a3ef6bbd56e1e5459aa2ac8a85b059
eepymeowers System <106939391+eepymeowers@users.noreply.github.com>
2025-02-13 18:42:04 -0500

    Update and rename lineage_guacamole.mk to lineage_guacamoles.mk

--- lineage_guacamoles.mk
+++ lineage_guacamoles.mk
@@ -9,15 +9,15 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 TARGET_SUPPORTS_OMX_SERVICE := false
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit from guacamole device
-$(call inherit-product, device/oneplus/guacamole/device.mk)
+# Inherit from guacamoles device
+$(call inherit-product, device/oneplus/guacamoles/device.mk)
 
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_guacamole
-PRODUCT_DEVICE := guacamole
+PRODUCT_NAME := lineage_guacamoles
+PRODUCT_DEVICE := guacamoles
 PRODUCT_MANUFACTURER := OnePlus
 PRODUCT_MODEL := GM1911
 PRODUCT_BRAND := OnePlus
